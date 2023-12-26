5e3ebf8390a1e398fbbd391e95eb576db6d77245
Robbie Nichols <nichols.robbie@gmail.com>
2023-12-25 23:38:09 -0600

    add created/modified dates/users to models

--- cardbox/admin.py
+++ cardbox/admin.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
@@ -9,27 +10,30 @@ from .models import (
     UserProfile,
     Card,
     UserCategoryOptions,
+    UserCardPrayedLog,
 )
-
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
+    fk_name = "user"
     can_delete = False
-    readonly_fields = ("user", "prayer_deck_last_updated")
+    readonly_fields = ("user", "prayer_deck_last_updated", "created_date", "modified_date")
 
 
 class UserCardInline(admin.TabularInline):
     model = UserCard
-    can_delete = True
-    fields = ("card", "answered", "hidden", "last_prayed", "in_prayer_deck")
+    fk_name = "user"
+    can_delete = False
+    fields = ("card", "in_prayer_deck", "answered", "hidden", "last_prayed")
     readonly_fields = ("card", )
     extra = 0
     sortable_by = ("card__category", "answered", "hidden")
     search_fields = ("card__title",)
 
 
-class UserCategoryScheduleInline(admin.TabularInline):
+class UserCategoryOptionsInline(admin.TabularInline):
     model = UserCategoryOptions
+    fk_name = "user"
     can_delete = False
     max_num = 0
     fields = ("category", "enabled", "pray_all_cards")
@@ -40,9 +44,19 @@ class UserAdmin(BaseUserAdmin):
     list_prefetch_related = ("userprofile", "usercard", "usercategoryschedule", "prayer")
     inlines = [
         UserProfileInline,
-        UserCategoryScheduleInline,
+        UserCategoryOptionsInline,
         UserCardInline,
     ]
+
+    def save_formset(self, request: Any, form: Any, formset: Any, change: Any) -> None:
+        # All of the inline formsets have the created_by and modified_by fields
+        instances = formset.save(commit=False)
+        for instance in instances:
+            if instance.pk is None:
+                instance.created_by = request.user
+            instance.modified_by = request.user
+            instance.save()
+        return super().save_formset(request, form, formset, change)
 
 
 admin.site.unregister(User)  # So that we can add the inlines above
@@ -55,4 +69,26 @@ class CardAdmin(TranslationAdmin):
     list_filter = ("category",)
     search_fields = ("title", "scripture", "text")
 
-admin.site.register(UserCard)
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(UserCard)
+class UserCardAdmin(admin.ModelAdmin):
+    fk_name = 'user'
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(UserCardPrayedLog)
+class UserCardPrayedLogAdmin(admin.ModelAdmin):
+    list_display = ("usercard", "date_prayed")
+    list_filter = ("usercard__card__category", "usercard__user")
+    sortable_by = ("usercard__card__category", "usercard__user", 'date_prayed')
+    readonly_fields = ("usercard", "date_prayed")
--- cardbox/forms.py
+++ cardbox/forms.py
@@ -7,23 +7,55 @@ class UserCategoryOptionsForm(forms.ModelForm):
         model = UserCategoryOptions
         fields = ["enabled", "pray_all_cards"]
 
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.modified_by = self.request.user
+        if commit:
+            instance.save()
+        return instance
+
 
 UserCategoryOptionsFormSet = forms.modelformset_factory(
     UserCategoryOptions, form=UserCategoryOptionsForm, extra=0
 )
-
-UserCardNoteFormSet = forms.inlineformset_factory(
-    UserCard, UserCardNote, fields=("note",), extra=1
-)
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ["cards_per_day"]
 
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.modified_by = self.request.user
+        if commit:
+            instance.save()
+        return instance
+
 
 class UserCardNoteForm(forms.ModelForm):
     class Meta:
         model = UserCardNote
         fields = ["note"]
+        
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.modified_by = self.request.user
+        if commit:
+            instance.save()
+        return instance
+    
+UserCardNoteFormSet = forms.inlineformset_factory(
+    UserCard, UserCardNote, fields=("note",), form=UserCardNoteForm, extra=1
+)
