0e237f324819ecbe7b48c4425a5525ce76bc9908
Robbie Nichols <nichols.robbie@gmail.com>
2024-01-06 12:49:39 -0600

    fix load_cards after spreadsheet change

--- prayerapp/management/commands/load_cards.py
+++ prayerapp/management/commands/load_cards.py
@@ -1,6 +1,8 @@
 import csv
+
 import requests
 from django.core.management.base import BaseCommand, CommandError
+
 from prayerapp.models import Card
 
 
@@ -32,8 +34,8 @@ class Command(BaseCommand):
                         category=category,
                         title=line[0].strip(),
                         scripture=line[1].strip(),
-                        title_es=line[2].strip(),
-                        scripture_es=line[3].strip(),
+                        title_es=line[4].strip(),
+                        scripture_es=line[5].strip(),
                     )
                     card.save()
                     self.stdout.write(
