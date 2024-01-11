89ab1c90d49ae931d734295ea58832d767fb1829
Kenan Casey <kenancasey@gmail.com>
2024-01-11 15:56:12 -0600

    Improve styling and swiper

--- spa-prototype/src/components/PrayerDeck.tsx
+++ spa-prototype/src/components/PrayerDeck.tsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
 
-const PrayerDeck = () => {
-  return (
-    <>
-    <h2>Today's Prayers</h2>
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Keyboard, A11y } from 'swiper/modules';
 
-    
 
-    </>
-  )
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './swiper.css'
+
+import UserCard from "./UserCard";
+
+export interface UserCardProps {
+  id: number;
+  card: string;
+  // "usercardnote_set": [],
+  // "answered": false,
+  // "hidden": false,
+  // "in_prayer_deck": false
 }
 
-export default PrayerDeck
+const PrayerDeck = () => {
+  const [userCards, setUserCards] = useState<UserCardProps[]>([]);
+
+  useEffect(() => {
+    // Function to fetch data from the JSON file
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/usercards.json");
+        const data = await response.json();
+        console.log(data.results);
+        setUserCards(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Call the fetchData function
+    fetchData();
+  }, []); // Empty dependency array ensures that useEffect runs only once
+
+  return (
+    <>
+      <h2>Today's Prayers</h2>
+
+      <Swiper
+
+        className="p-16 bg-teal-900"
+        modules={[Navigation, Pagination, Keyboard, A11y]}
+        spaceBetween='14rem'
+        slidesPerView={1}
+        onSlideChange={(item) => console.log(item)}
+        onSwiper={(swiper) => console.log(swiper)}
+        navigation
+        pagination={{ clickable: true }}
+        keyboard
+      >
+        {userCards.map((item) => (
+          <SwiperSlide key={item.id} >
+            <UserCard usercard={item} />  
+          </SwiperSlide>
+        ))}
+      </Swiper>     
+
+        <Button className="text-lg"> 
+        <BookmarkIcon width="16" height="16" />
+        Finish Prayer Session</Button>
+
+    </>
+  );
+};
+
+export default PrayerDeck;
--- spa-prototype/src/components/UserCard.tsx
+++ spa-prototype/src/components/UserCard.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
-const UserCard = () => {
-  return (
-    <div>UserPrayerCard</div>
-  )
+import { Badge, Text} from "@radix-ui/themes";
+import { UserCardProps } from "./PrayerDeck";
+interface Props {
+  usercard: UserCardProps;
 }
 
-export default UserCard
+const UserCard = ({ usercard }: Props) => {
+  return (
+    <div className="bg-gradient-to-bl from-teal-200 to-lime-200 h-50 py-12 px-16">
+
+      {/* <p className="text-base font-light uppercase pb-4">Names of God</p> */}
+      <Badge color="jade" 
+        radius="small"
+        size="2" 
+        mb="4"
+        variant="soft"
+        className="uppercase"> 
+        Names of God 
+      </Badge>
+
+      <h3 className='text-3xl pb-8'>{usercard.card}</h3>
+
+      <div className="border-l-8 border-teal-600 pl-4">
+        <p className="text-lg ">
+          The LORD is my high ridge, my stronghold, my deliverer. My God is my
+          rocky summit where I take shelter, my shield, the horn that saves me,
+          and my refuge.
+        </p>
+        <p className="uppercase text-sm pt-4">Psalm 18:2 (NET)</p>
+      </div>
+
+
+      <Text color="jade"
+        as="p"
+        size="2"
+        mt="8"
+        className="">
+        Give God glory or adoration that comes to mind.
+        </Text>
+
+    </div>
+  );
+};
+
+export default UserCard;
