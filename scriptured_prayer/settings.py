b6f573eb8e22c56aafd9ad9569b239c518e7651d
Robbie Nichols <nichols.robbie@gmail.com>
2024-01-11 20:03:28 -0600

    disable pagination on api

--- scriptured_prayer/settings.py
+++ scriptured_prayer/settings.py
@@ -159,6 +159,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25
 }
