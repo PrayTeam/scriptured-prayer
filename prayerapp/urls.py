4064a46297a4eb51debdf68728527318252835b6
Asher Lloyd <asher.jo.lloyd@gmail.com>
2024-01-14 00:01:47 -0600

    feat: integrate react app into django site

--- prayerapp/urls.py
+++ prayerapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.utils.translation import gettext_lazy as _
 from rest_framework import routers
 
@@ -8,7 +8,6 @@ router.register(r'usercards', views.UserCardViewSet, basename="usercards")
 
 app_name = "prayerapp"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="prayerapp"),
     path('api/', include(router.urls)),
     path(_("cards/"), views.UserCardListView.as_view(), name="usercard_list"),
     path(_("prayerdeck/"), views.PrayerDeckView.as_view(), name="prayer"),
@@ -16,6 +15,8 @@ urlpatterns = [
         _("profile/"), views.UserProfileUpdateView.as_view(), name="userprofile_update"
     ),
     path("card/<int:pk>/", views.UserCardDetailView.as_view(), name="usercard_detail"),
+    # to make react router play nice
+    re_path(r".*", views.IndexView.as_view(), name="prayerapp"),
 ]
 
 urlpatterns += router.urls
