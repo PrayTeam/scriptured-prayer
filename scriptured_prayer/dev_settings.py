690af2b7f722030867bf04817e5f9920655a844a
Robbie Nichols <nichols.robbie@gmail.com>
2024-03-09 22:01:49 -0600

    fix: ansible and django code for prod

--- scriptured_prayer/dev_settings.py
+++ scriptured_prayer/dev_settings.py
@@ -24,3 +24,40 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",    
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # current React app dev server
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+# DRF Spectacular settings
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Scriptured Prayer API",
+    "DESCRIPTION": "Easy access to the backend for the Scriptured Prayer app.",
+    "VERSION": "0.4.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+}
+
+REST_FRAMEWORK["DEFAULT_SCHEMA_CLASS"] = "drf_spectacular.openapi.AutoSchema"
+
+# For debug_toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+GRAPH_MODELS = {
+    "all_applications": True,
+    "group_models": True,
+}
