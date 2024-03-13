a595f1228bbdd8d771a2b957e8602089cd1f1565
GreeneGunnar14 <bryce.greene@students.fhu.edu>
2024-03-13 00:29:06 -0500

    chore: get privacy policy up to date with google doc

--- scriptured-prayer-components/src/views/Privacy.tsx
+++ scriptured-prayer-components/src/views/Privacy.tsx
@@ -1,133 +1,237 @@
-import { useState } from "react";
-import DropdownButton from "~/components/DropdownButton";
-import PrivacySection from "~/components/PrivacySection";
-
 const Privacy = () => {
-  const [visibleSections, updateVisibleSections] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
-
-  const onClick = (section_id: number) => {
-    const updatedSections = [...visibleSections];
-    updatedSections[section_id] = !updatedSections[section_id];
-    console.log(updatedSections);
-    updateVisibleSections(updatedSections);
-  };
-
   return (
     <div className="bg-snowgrass h-fit w-full flex justify-center min-h-full">
-      <div className="flex flex-col items-center max-w-full md:max-w-[1200px] md:w-3/4 p-8 ">
-        <div className="flex flex-col items-center p-4 max-w-full">
-          <h1 className="text-4xl md:text-6xl text-center font-bold uppercase">
-            Scriptured Prayer
-          </h1>
-          <h2 className="text p-4 text-2xl md:text-3xl font-semibold">
-            Privacy Policy
-          </h2>
+      <div className="flex flex-col items-center md:max-w-[1200px] p-8 ">
+        <h2 className="text p-4 text-4xl md:text-3xl font-bold">
+          Privacy Policy
+        </h2>
+
+        <div className="w-4/5 flex justify-center p-4">
+          <p className="text-lg">
+            Prayer is important to us, and so is your privacy. This privacy
+            policy explains how Scriptured Prayer uses the personal data we
+            collect from you when you use our application.
+          </p>
         </div>
-        <div className="w-4/5 flex justify-center">
-          <div className="max-w-full md:w-9/12 flex justify-center p-4">
-            <p className="text-lg">
-              Prayer is important to us, and so is your privacy. This privacy
-              policy explains how Scriptured Prayer uses the personal data we
-              collect from you when you use our application.
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">What Data Do We Collect?</h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">
+              Scriptured Prayer collects the following data:
+            </p>
+            <ul className="list-disc list-inside">
+              <li className="">
+                Personal Identification (Name, username, email address)
+              </li>
+              <li className="">
+                Personal prayer request information (Name, reason for prayer,
+                whether the prayer was answered)
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            How Do We Collect Your Data?
+          </h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">
+              You directly provide Scriptured Prayer with most of the data we
+              collect. We collect and process data when you:
+            </p>
+            <ul className="list-disc list-inside">
+              <li>Register online for a user account</li>
+              <li>Use or view our application via your device's cookies</li>
+              <li>Input data for a specific prayer request</li>
+            </ul>
+            <p>Scriptured Prayer may also receive data from Google Analytics</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">How Will We Use Your Data?</h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">Scriptured Prayer will use this data to:</p>
+            <ul className="list-disc list-inside">
+              <li>Process and track the prayer requests you enter</li>
+              <li>Verify your identity as a legitimate user</li>
+              <li>
+                Enable you to participate in Scriptured Prayer's user features
+              </li>
+              <li>
+                Understand how people use and move around our application to
+                make it more intuitive
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            How Do We Share Your Information?
+          </h3>
+          <div className="max-w-full text-lg my-4">
+            <p>
+              We share non-personal information with third parties. We share
+              your device identifiers with providers of data analytics, hosting
+              services, business communication, and collaboration tools, data
+              storage services, product engineering and design tools, and
+              infrastructure tools.
             </p>
           </div>
         </div>
-        <div className="w-full h-px bg-black m-4" /> {/* Horizontal rule */}
-        <div className="flex flex-col w-4/5 p-4 overflow-hidden">
-          <PrivacySection
-            title="What Data Do We Collect?"
-            button={
-              <DropdownButton
-                open={visibleSections[0]}
-                onClick={() => onClick(0)}
-              />
-            }
-            visible={visibleSections[0]}
-          >
-            <div className="max-w-full text-lg my-4">
-              <p>Scriptured Prayer collects the following data:</p>
-              <ul className="list-disc list-inside">
-                <li className="">
-                  Personal Identification (Name, username, email address)
-                </li>
-                <li className="">
-                  Personal prayer request information (Name, reason for prayer,
-                  whether the prayer was answered)
-                </li>
-              </ul>
-            </div>
-          </PrivacySection>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            How Long Will We Store Your Information?
+          </h3>
+          <div className="max-w-full text-lg my-4">
+            <p>
+              We will retain your information for as long as we are required to
+              provide the Service to you, as required to fulfill our legal
+              requirements and defend legal claims. After you have terminated
+              your use of our services, we store your information in an
+              aggregated and anonymized format.
+            </p>
+          </div>
         </div>
-        <div className="w-full h-px bg-black m-4" /> {/* Horizontal rule */}
-        <div className="flex flex-col w-4/5 p-4 overflow-hidden">
-          <PrivacySection
-            title="How Do We Collect Your Data?"
-            button={
-              <DropdownButton
-                open={visibleSections[1]}
-                onClick={() => onClick(1)}
-              />
-            }
-            visible={visibleSections[1]}
-          >
-            <div className="max-w-full text-lg my-4">
-              <p>
-                You directly provide Scriptured Prayer with most of the data we
-                collect. We collect and process data when you:
-              </p>
-              <ul className="list-disc list-inside">
-                <li>Register online for a user account</li>
-                <li>Use or view our application via your device's cookies</li>
-                <li>Input data for a specific prayer request</li>
-              </ul>
-              <p>
-                Scriptured Prayer may also receive data from Google Analytics
-              </p>
-            </div>
-          </PrivacySection>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            What Are Your Data Protection Rights?
+          </h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">
+              Scriptured Prayer wants to make you fully aware of all of your
+              data protection rights. Every user is entitled to the following:
+            </p>
+            <ul className="list-disc list-inside">
+              <li>
+                The right to access: You have the right to ask us to access the
+                personal data we hold about you and be provided with certain
+                information about how we use your personal data and who we share
+                it with.
+              </li>
+              <li>
+                The right to rectify: You also have the right to ask us to
+                correct your data where it is inaccurate or incomplete.
+              </li>
+              <li>
+                The right to portability: You have the right to ask us that a
+                copy of your personal data be transferred to another
+                organization or to you, under certain conditions. The right to
+                deletion: Under certain conditions, you have the right to
+                request that we erase your data.
+              </li>
+              <li>
+                The right to restriction: You have the right to request that we
+                restrict the processing of your personal data, under certain
+                conditions.
+              </li>
+            </ul>
+            <p className="pb-4">
+              To exercise any of the rights above, please contact us at: [insert
+              email].
+            </p>
+          </div>
         </div>
-        <div className="w-full h-px bg-black m-4" /> {/* Horizontal rule */}
-        <div className="flex flex-col w-4/5 p-4 overflow-hidden">
-          <PrivacySection
-            title="How Will We Use Your Data?"
-            button={
-              <DropdownButton
-                open={visibleSections[2]}
-                onClick={() => onClick(2)}
-              />
-            }
-            visible={visibleSections[2]}
-          >
-            <div className="max-w-full text-lg my-4">
-              <p>Scriptured Prayer will use this data to:</p>
-              <ul className="list-disc list-inside">
-                <li>Process and track the prayer requests you enter</li>
-                <li>Verify your identity as a legitimate user</li>
-                <li>
-                  Enable you to participate in Scriptured Prayer's user features
-                </li>
-                <li>
-                  Understand how people use and move around our application to
-                  make it more intuitive
-                </li>
-              </ul>
-              <p>
-                Scriptured Prayer may also receive data from Google Analytics.
-              </p>
-            </div>
-          </PrivacySection>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">Data Security</h3>
+          <p className="my-4">
+            In the event of a data breach, Scriptured Prayer will... [FIXME]
+          </p>
         </div>
-        <div className="w-full h-px bg-black m-4" /> {/* Horizontal rule */}
-        <div className="flex w-4/5 p-4">
-          <h3 className="text-2xl font-semibold">What Data Do We Collect?</h3>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">What Are Cookies?</h3>
+          <p className="my-4">
+            Http cookies, also known as web cookies, browser cookies, or simply
+            “cookies,” are text files placed in your browser when you visit a
+            website or application. These files collect standard information and
+            visitor behavior. When you visit our application, we may
+            automatically store such information on your device.
+          </p>
         </div>
-        <div className="w-full h-px bg-black m-4" /> {/* Horizontal rule */}
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">How We Use Cookies</h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">
+              Scriptured Prayer uses cookies in a range of ways to improve your
+              experience on our application, including:
+            </p>
+            <ul className="list-disc list-inside">
+              <li>Keeping you signed in</li>
+              <li>Understanding how you use our application</li>
+              <li>Allowing you to access private prayer requests</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            What Types of Cookies Do We Use?
+          </h3>
+          <div className="max-w-full text-lg my-4">
+            <p className="mb-2">
+              We use first-party and third-party (Google Analytics) tracking
+              technologies to perform the following functions:
+            </p>
+            <ul className="list-disc list-inside">
+              <li>
+                Essential Tracking Technologies which are beneficial to
+                understanding our platform and user base. Tracking data
+                collection is anonymous.
+              </li>
+              <li>
+                Functionality Tracking Technologies, which allow us to remember
+                choices you make and provide personalized features when you are
+                logged in.
+              </li>
+              <li>
+                Performance Tracking Technologies, which enable us to collect
+                information about your activity on our application, including
+                behavioral data and content engagement. These cookies allow us
+                to provide you with a better user experience.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">Managing Cookies</h3>
+          <p className="my-4">
+            Some cookies are necessary for delivering our application to
+            visitors, but others can be turned off. You have the right to choose
+            whether or not to accept these optional cookies. If you would like
+            to opt-in or opt-out of using cookies, you should be able to do so
+            using your browser. You can review your cookie settings at any time.
+          </p>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">
+            Changes to Our Privacy Policy
+          </h3>
+          <p className="my-4">
+            We reserve the right to update this policy at any time. If we make
+            any changes to the way we handle your data, we will amend this page
+            to reflect those changes. The privacy policy was last updated on
+            February 1, 2024.
+          </p>
+        </div>
+
+        <div className="flex flex-col w-4/5 p-4">
+          <h3 className="text-2xl font-semibold">How to Contact Us</h3>
+          <p className="my-4">
+            If you have questions about our privacy policy or the data we hold,
+            or if you would like to exercise one of your data protection rights,
+            please do not hesitate to contact us. Email us at: [insert email]
+          </p>
+        </div>
       </div>
     </div>
   );
