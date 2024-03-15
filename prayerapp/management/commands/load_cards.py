d7b41da0042c69f3acdb8cefa125458a0fa62a33
Robbie Nichols <nichols.robbie@gmail.com>
2024-03-15 14:20:38 -0500

    fix: load_cards descriptions

--- prayerapp/management/commands/load_cards.py
+++ prayerapp/management/commands/load_cards.py
@@ -40,8 +40,10 @@ class Command(BaseCommand):
                         category=category,
                         title=line[0].strip(),
                         scripture=line[1].strip(),
-                        title_es=line[2].strip(),
-                        scripture_es=line[3].strip(),
+                        description=line[3].strip(),
+                        title_es=line[4].strip(),
+                        scripture_es=line[5].strip(),
+                        description_es=line[7].strip(),
                     )
                     card.save()
                     self.stdout.write(self.style.SUCCESS('Successfully loaded card "%s"' % line[0]))
