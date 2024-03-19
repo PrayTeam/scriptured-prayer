1717968db00681ce7ba52df025896484eaa6ea27
sflowers51 <samuel.flowers@students.fhu.edu>
2024-03-19 11:54:33 -0500

    feat: added 3 more questions to faq.tsx

--- scriptured-prayer-components/src/App.tsx
+++ scriptured-prayer-components/src/App.tsx
@@ -2,8 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
-import { Info, About, Home, Login, NotFound, Settings } from "./views";
-import { Faq } from "./views/Faq";
+import { Info, About, Home, Login, NotFound, Settings, Faq } from "./views";
 import { ProtectedRoutes } from "./components";
 import { useLocalStorage } from "./hooks";
 import { ProfileContext } from "./hooks";
