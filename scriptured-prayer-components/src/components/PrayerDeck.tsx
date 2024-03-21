4329159b99f0d3bec15a2b886314638e95f1e852
LouviersJ <lou.joseph@students.fhu.edu>
2024-03-21 15:54:45 -0500

    fix: swiper pagenation as fraction

--- scriptured-prayer-components/src/components/PrayerDeck.tsx
+++ scriptured-prayer-components/src/components/PrayerDeck.tsx
@@ -44,7 +44,10 @@ function PrayerDeck() {
           modules={[Navigation, Pagination, Keyboard, A11y]}
           spaceBetween="14rem"
           slidesPerView={1}
-          pagination={{ clickable: true }}
+          pagination={{
+            clickable: true,
+            type: "fraction",
+          }}
           keyboard
         >
           {cards.map((card) => (
