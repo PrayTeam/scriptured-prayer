d12d1defad2ab83bafe1404f0879b30d03bfe179
Bryce <greene8616@gmail.com>
2024-04-16 14:37:13 -0500

    fix: update styling on daily deck cover

--- scriptured-prayer-components/src/components/DailyDeck.tsx
+++ scriptured-prayer-components/src/components/DailyDeck.tsx
@@ -37,25 +37,29 @@ export function DailyDeck() {
   const date_string = `${day_names[today.getDay()]}, ${month_names[today.getMonth()]} ${date}${date_suffix}, ${today.getFullYear()}`;
 
   return (
-    <div className="bg-lichen bg-opacity-80 max-h-fit py-36 flex justify-center items-center pl-0 lg:pl-20 pt-52 pb-20 lg:pt-32 lg:pb-32">
-      <div className="bg-gray px-8 lg:pl-10 pt-10 lg:pt-0 rounded-3xl flex flex-col justify-center items-center h-[400px] min-w-[350px] w-2/3 lg:w-auto lg:aspect-[13/8] relative shadow-lg">
+    <div className="bg-lichen text-lg md:text-2xl bg-opacity-80 max-h-fit py-36 flex justify-center items-center pl-0 lg:pl-20 pt-52 pb-20 lg:pt-32 lg:pb-32">
+      <div className="bg-gray px-8 lg:px-0 lg:pl-20 pt-10 lg:pt-0 rounded-3xl flex flex-col justify-center items-center h-[400px] min-w-[350px] w-2/3 lg:w-auto lg:aspect-[13/8] relative shadow-lg">
         <img
           className="absolute h-44 sm:h-52 lg:h-64 aspect-[11/8] sm:aspect-[13/7] lg:aspect-square rounded-lg left-auto -top-24 sm:-top-32 lg:top-auto lg:-left-32"
           src={nature}
           alt="Man on mountain peak with forest backdrop and sunlight"
         />
-        <div className="w-full lg:w-1/2 flex flex-col gap-1 lg:items-start items-center font-medium">
-          <p className="mb-3">{date_string}</p>
-          <h2 className="font-bold text-3xl">Today's Daily Deck</h2>
-          <p className="text-lg text-center lg:text-left">
-            A group of verses that encourage praise and thanksgiving.
-          </p>
+        <div className="w-full lg:w-2/3 flex flex-col gap-6 lg:items-start items-center font-medium">
+          <p className="">{date_string}</p>
+          <div className="text-center lg:text-left">
+            <h2 className="font-bold text-3xl md:text-4xl">
+              Today's Daily Deck
+            </h2>
+            <p className="text-center lg:text-left">
+              A group of verses that encourage praise and thanksgiving.
+            </p>
+          </div>
           <a
             href="/prayer-decks/daily"
-            className="bg-leaf rounded-lg text-center text-white shadow-md shadow-night w-52 mt-8 p-2"
+            className="bg-leaf rounded-lg scale-100 hover:scale-110 ease-out duration-300 text-center text-white shadow-md shadow-night w-64 p-2"
           >
-            <p className="text-lg font">Pray</p>
-            <p>2 min | 5 cards</p>
+            <p className="text-2xl">Pray</p>
+            <p className="text-base">2 min | 5 cards</p>
           </a>
         </div>
       </div>
