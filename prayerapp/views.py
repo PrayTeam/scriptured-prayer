b4f9302501827f4a611c4a7c9d0adf427a4ee01f
Bryce Greene <69988106+GreeneGunnar14@users.noreply.github.com>
2024-06-06 22:58:16 -0500

    Feat/logging swipes (#109)
    * chore: create UserCardsRequest and export it
    
    * chore: fix user card response interface
    
    * chore: update Card component to accept UserCardResponse props
    
    * chore: add add field to filter UserCards by card category id
    
    * chore: write useApi method to get usercards and log usercard swipes
    
    * feat: add swipe logging when user is authenticated
    
    * fix: fix 'loading...' text not being replaced after cards loaded
    
    * fix: add csrf token to logCard api method
    
    * fix: correct request url for logging card swipes
    
    * fix: fix __str__ method for UserCardPrayedLog
    
    ---------
    
    Co-authored-by: Asher Lloyd <asher.jo.lloyd@gmail.com>

--- prayerapp/views.py
+++ prayerapp/views.py
@@ -92,6 +92,7 @@ class UserCardFilter(django_filters.FilterSet):
         fields = {
             "card__category__name": ["exact"],
             "card__category__genre": ["exact"],
+            "card__category__id": ["exact"],
             "answered": ["exact"],
             "hidden": ["exact"],
             "in_prayer_deck": ["exact"],
@@ -161,7 +162,6 @@ class UserCardViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["post"])
     def log_prayer(self, request, pk=None, **kwargs):
-        print("hello from usercardviewset")
         usercard = self.get_object()
         if usercard.user != request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
