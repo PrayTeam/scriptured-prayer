40a2582f43b77a4de55bcf33d55e1a2904d574b7
Asher Lloyd <asherlloyd@users.noreply.github.com>
2024-08-24 13:04:18 -0500

    feat: general ui cleanup and tweaks

--- frontend/src/components/Icons.tsx
+++ frontend/src/components/Icons.tsx
@@ -1,7 +1,29 @@
 /// <reference types="vite-plugin-svgr/client" />
 import LeftArrowIcon from "~/assets/icons/left-arrow.svg?react";
+import RightArrowIcon from "~/assets/icons/right-arrow.svg?react";
+import LogoutIcon from "~/assets/icons/logout.svg?react";
+import LanguageIcon from "~/assets/icons/language.svg?react";
+import NotificationIcon from "~/assets/icons/notification.svg?react";
+import PrivacyIcon from "~/assets/icons/privacy.svg?react";
+import AddIcon from "~/assets/icons/add.svg?react";
+import LoadingIcon from "~/assets/icons/loading.svg?react";
+
 import DashboardIcon from "~/assets/icons/navigation/dashboard.svg?react";
 import DeckIcon from "~/assets/icons/navigation/deck.svg?react";
 import PrayerIcon from "~/assets/icons/navigation/prayer.svg?react";
+import SearchIcon from "~/assets/icons/navigation/search.svg?react";
 
-export { LeftArrowIcon, DashboardIcon, DeckIcon, PrayerIcon };
+export {
+  LeftArrowIcon,
+  RightArrowIcon,
+  LogoutIcon,
+  LanguageIcon,
+  NotificationIcon,
+  PrivacyIcon,
+  AddIcon,
+  LoadingIcon,
+  DashboardIcon,
+  DeckIcon,
+  PrayerIcon,
+  SearchIcon,
+};
