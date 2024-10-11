5c2cd3a4ce5fb5296f2d027fa278a1ed2e4ba7a9
Asher Lloyd <asherlloyd@users.noreply.github.com>
2024-10-11 01:22:43 -0500

    fix: correct incorrect padding value on nested screens

--- frontend/src/components/NestedScreen.tsx
+++ frontend/src/components/NestedScreen.tsx
@@ -8,7 +8,7 @@ export function NestedScreen({ className, ...props }: NestedScreenProps) {
   return (
     <div className={`h-full ${className}`} {...props}>
       <ContextualNavigation title={props.title} />
-      <div className="h-full pt-[57px] lg:mt-0">{props.children}</div>
+      <div className="h-full pt-[57px] lg:pt-0">{props.children}</div>
     </div>
   );
 }
