a7ff49ed2068ffe4baa36af5e7c719771a622682
Asher Lloyd <asherlloyd@users.noreply.github.com>
2024-11-03 21:02:33 -0600

    feat: add user daily decks stuff for someone's birthday

--- frontend/src/components/PrayerDeck.tsx
+++ frontend/src/components/PrayerDeck.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@radix-ui/themes";
 import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
@@ -8,66 +8,22 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import "~/swiper.css";
-import { useApi, useProfile, useRouteId } from "~/hooks";
-import { CardResponse } from "~/api/models/responses";
-import { CategoryResponse } from "~/api/models/responses";
-import { Card } from "./Card";
 import { Loader } from "./Loader";
 import { NestedScreen } from "./NestedScreen";
 
-export function PrayerDeck() {
-  const api = useApi();
-  const routeId = useRouteId();
+interface PrayerDeckProps {
+  title: string;
+  children: ReactNode;
+  loading: boolean;
+  onCardChange?: (swiper: SwiperClass) => void;
+}
+
+export function PrayerDeck(props: PrayerDeckProps) {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  const [countdownElapsed, setCountdownElapsed] = useState(false);
-  const [cards, setCards] = useState<CardResponse[]>([]);
-  const [category, setCategory] = useState<CategoryResponse>();
-  const { profile } = useProfile();
-  const [activeCardId, setActiveCardId] = useState<number>();
-  const [authenticated] = useState(profile && profile.authenticated);
-
-  const handleSwipe = (swiper: SwiperClass) => {
-    // skip the "inspiration" card at the beginning
-    if (!authenticated || swiper.previousIndex === 0) return;
-    activeCardId && api.userCards.logCard(activeCardId);
-    const {
-      dataset: { id },
-    } = swiper.slides[swiper.activeIndex];
-    id && setActiveCardId(+id);
-  };
-
-  useEffect(() => {
-    // delay at least 1 second before we unhide the "now praying" message curtain
-    setTimeout(() => setCountdownElapsed(true), 1000);
-
-    if (routeId) {
-      (async () => {
-        const cardsApi = authenticated ? api.userCards : api.cards;
-        const cardsApiRequest = authenticated
-          ? { card__category__id: routeId }
-          : { category__id: routeId };
-
-        Promise.all([cardsApi.all(cardsApiRequest), api.categories.all()])
-          .then(([_cards, categories]) => {
-            setCards(_cards);
-            setCategory(categories.find((c) => c.id === routeId));
-            if (authenticated) setActiveCardId(_cards[0].id); // todo
-          })
-          .catch((error) => console.error(error))
-          .finally(() => {
-            setLoading(false);
-            setCountdownElapsed(true);
-          });
-      })();
-    }
-  }, []);
-
-  if (!routeId) return <>Error: an id must be provided.</>;
 
   return (
     <>
-      <NestedScreen title="Prayer Deck">
+      <NestedScreen title={props.title}>
         <div className="bg-ghost h-full">
           <div className="flex flex-col">
             <Swiper
@@ -80,51 +36,48 @@ export function PrayerDeck() {
                 type: "fraction",
               }}
               keyboard
-              onActiveIndexChange={handleSwipe}
+              onActiveIndexChange={props.onCardChange}
             >
-              {category && (
-                <>
-                  <SwiperSlide>
-                    <Card
-                      focus
-                      category={category.name}
-                      title="Inspiration"
-                      body={category.inspiration}
-                      cardCount={category.card_count}
-                    />
-                  </SwiperSlide>
-                  {cards.map((card) => (
-                    <SwiperSlide key={card.id}>
-                      <Card
-                        category={card.category}
-                        title={card.title}
-                        body={card.scripture_text
-                          .map((st) => st.text)
-                          .join(" ")}
-                        scripture={card.scripture}
-                        instruction={card.instruction}
-                      />
-                    </SwiperSlide>
-                  ))}
-                  <SwiperSlide>
-                    <div className="flex justify-center items-center h-full">
-                      <Button
-                        onClick={() => navigate("/dashboard")}
-                        size="4"
-                        className="w-[100px] mx-auto mt-4 bg-lichen transition-opacity ease-in"
-                      >
-                        Done
-                      </Button>
-                    </div>
-                  </SwiperSlide>
-                </>
-              )}
+              {/* <SwiperSlide>
+                <Card
+                  focus
+                  category={category.name}
+                  title="Inspiration"
+                  body={category.inspiration}
+                  cardCount={category.card_count}
+                />
+              </SwiperSlide>
+              {cards.map((card) => (
+                <SwiperSlide key={card.id}>
+                  <Card
+                    category={card.category}
+                    title={card.title}
+                    body={card.scripture_text
+                      .map((st) => st.text)
+                      .join(" ")}
+                    scripture={card.scripture}
+                    instruction={card.instruction}
+                  />
+                </SwiperSlide>
+              ))} */}
+              {props.children}
+              <SwiperSlide>
+                <div className="flex justify-center items-center h-full">
+                  <Button
+                    onClick={() => navigate("/dashboard")}
+                    size="4"
+                    className="w-[100px] mx-auto mt-4 bg-lichen transition-opacity ease-in"
+                  >
+                    Done
+                  </Button>
+                </div>
+              </SwiperSlide>
             </Swiper>
           </div>
         </div>
       </NestedScreen>
       <div
-        style={{ opacity: loading || !countdownElapsed ? 1 : 0 }}
+        style={{ opacity: props.loading ? 1 : 0 }}
         className="fixed flex flex-col w-full h-full bg-black text-white justify-center items-center transition-opacity ease-in top-0 left-0 bottom-0 right-0 z-10 pointer-events-none"
       >
         <h1 className="text-lg font-bold mb-4">Loading prayers</h1>
--- frontend/src/views/DailyDeck.tsx
+++
@@ -1,3 +0,0 @@
-export function DailyDeck() {
-  return <div></div>;
-}
